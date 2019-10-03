6313a850a4d8235e750ce721c08cae63fecba67b
umekow <umekolashun@gmail.com>
2019-10-03 18:07:06 -0500

    started on dashboard component

--- components/Display.js
+++ components/Display.js
@@ -4,7 +4,13 @@ const Display = props => {
 
     return (
         <div className="display">
-            
+            <div className="balls">
+                <span>{props.balls}</span>
+            </div>
+
+            <div className="strikes">
+                <span>{props.strikes}</span>
+            </div>
         </div>
     )
 }
